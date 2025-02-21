c67ecd007d7758ff92d3adafb32769b12f09d4a5
samu067 <sunarsamun653@gmail.com>
2025-02-21 22:29:01 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,90 @@
+window.addEventListener('load', () => {
+  // Once the page is fully loaded, hide the loader and show the content
+  document.body.classList.add('loaded');
+});
+
+const menuBtn = document.getElementById("menu-btn");
+if (!menuBtn) {
+  console.error("Element with id 'menu-btn' not found in the HTML document.");
+}
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
+
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
+
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
+
+const scrollRevealOption = {
+  distance: "50px",
+  origin: "bottom",
+  duration: 1000,
+};
+
+ScrollReveal().reveal(".about__container .section__header", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".about__container .section__description", {
+  ...scrollRevealOption,
+  delay: 500,
+  interval: 500,
+});
+ScrollReveal().reveal(".about__container img", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".service__container .section__header", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".service__container .section__description", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".service__card", {
+  duration: 1000,
+  delay: 1000,
+  interval: 500,
+});
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+ScrollReveal().reveal(".blog__content .section__header", {
+  ...scrollRevealOption,
+});
+ScrollReveal().reveal(".blog__content h4", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+ScrollReveal().reveal(".blog__content p", {
+  ...scrollRevealOption,
+  delay: 1000,
+});
+ScrollReveal().reveal(".blog__content .blog__btn", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+const instagram = document.querySelector(".instagram__flex");
+
+const originalChildren = Array.from(instagram.children);
+const numberOfChildren = originalChildren.length;
+
+for (let i = 0; i < numberOfChildren; i++) {
+  const item = originalChildren[i];
+  const duplicateNode = item.cloneNode(true);
+  duplicateNode.setAttribute("aria-hidden", true);
+  instagram.appendChild(duplicateNode);
+}
